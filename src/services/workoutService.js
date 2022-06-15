0ef9d31cc86a04cd836688222b3837a3c05094b4
Ana Muraya <Anamuraya9@gmail.com>
2022-06-16 00:37:51 +0300

    Added the create functionality

--- src/services/workoutService.js
+++ src/services/workoutService.js
@@ -1,34 +1,43 @@
-const Workout = require('../database/Workout');
+const { v4: uuidv4 } = require("uuid");
+const Workout = require("../database/Workout");
 
 //get all workouts
 const getAllWorkouts = () => {
-
-    const allWorkouts = Workout.getAllWorkouts();
-    return allWorkouts;
-}
+  const allWorkouts = Workout.getAllWorkouts();
+  return allWorkouts;
+};
 //get a single workout
 const getSingleWorkout = () => {
-    return `<h3>Hello from this workout!</h3>`;
-}
+  return `<h3>Hello from this workout!</h3>`;
+};
 //create a new workout
-const createNewWorkout = () => {
-    return `<h3>Creating a new workout!</h3>`;
-}
+const createNewWorkout = (newWorkout) => {
+  const workoutToInsert = {
+    ...newWorkout,
+    id: uuidv4(),
+    createdAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+    updatedAt: new Date().toLocaleString("en-US", { timeZone: "UTC" }),
+  };
+
+  const createdWorkout = Workout.createNewWorkout(workoutToInsert);
+
+  return createdWorkout;
+};
 
 //update a workout
 const updateSingleWorkout = () => {
-    return `<h3>Updating existing workout!</h3>`;
-}
+  return `<h3>Updating existing workout!</h3>`;
+};
 
 //delete a workout
 const deleteSingleWorkout = () => {
-    return `<h3>Deleting an existing workout!</h3>`;
-}
+  return `<h3>Deleting an existing workout!</h3>`;
+};
 
 module.exports = {
-    getAllWorkouts,
-    getSingleWorkout,
-    createNewWorkout,
-    updateSingleWorkout,
-    deleteSingleWorkout
-}
+  getAllWorkouts,
+  getSingleWorkout,
+  createNewWorkout,
+  updateSingleWorkout,
+  deleteSingleWorkout,
+};
